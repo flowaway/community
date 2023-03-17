17773c2b3bd2eed9b38aa72f7565cd49e05ceefd
maplewind <1026094471@qq.com>
2023-03-17 23:48:47 +0800

    init repo

---
+++ src/main/java/com/example/maplewind/MaplewindApplication.java
@@ -0,0 +1,13 @@
+package com.example.maplewind;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MaplewindApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MaplewindApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/maplewind/MaplewindApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.maplewind;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MaplewindApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
